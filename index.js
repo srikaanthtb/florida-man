581d3c3fe9b1444c19ddc8ab34b8590c10169040
srikaanthtb <srikaanth.t.b@gmail.com>
2021-07-24 16:24:07 +0100

    cors fix

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors())
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/index.html')
